58b40f9ef8cb629299721e9a6be2b9843dd60ab4
uk-ar <yuuki.ari@gmail.com>
2023-09-03 06:57:34 +0900

    meat spec

--- client/main.cpp
+++ client/main.cpp
@@ -9,42 +9,48 @@
 #include <netinet/in.h> // for struct_in
 #include <arpa/inet.h> // for inet_aton
 #include <iostream>
+#include <filesystem>
 
 #define PORT 8080
 #define BUFF_SIZE 1024
 using namespace std;
+namespace fs = std::filesystem;
 
 // > client read
 // content
 // > client write "hello world"
 // ok
-void print_usage(string file_name){
+void print_usage(fs::path path){
+    string file_name = path.filename();
+    cout << "usage:" << endl;
     cout << file_name << " ip read" << endl;
     cout << file_name << " ip write content" << endl;
+    cout << file_name << " ip insert content position" << endl << endl;
+    cout << "example:" << endl;
+    cout << file_name << " 127.0.0.1 read              'read data from localhost'" << endl;
+    cout << file_name << " 127.0.0.1 write content     'write \"content\" to the file in localhost'" << endl;
+    cout << file_name << " 127.0.0.1 insert content 3  'insert \"content\" at offset 3 from the beginning" << endl;
 }
 int main(int argc,const char *argv[]){
     vector<string> args(argv, argv + argc);
-    if(args.size() < 3){
+    string command = args[2];
+    //cout << args[0] << endl;
+    if((args.size() < 3) ||
+       (command == "write" && args.size()  != 4) ||
+       (command == "insert" && args.size() != 5)){
         cerr << "Too few args" << endl;
         print_usage(args[0]);
         return EXIT_FAILURE;
     }
-    // cout << argv[0] <<endl;
+
     struct  in_addr sin_addr;
     if (inet_aton(argv[1], &sin_addr) == 0){
             cerr << "Invalid IP address: " << argv[1] << " : " << strerror(errno) << endl;
         return EXIT_FAILURE;
     }
-    string command = args[2];
     
-    if(command != "read" && command != "write"){
+    if(command != "read" && command != "write" && command != "insert"){
         cerr << "Invalid command: " << args[2] << endl;
-        print_usage(args[0]);
-        return EXIT_FAILURE;
-    }
-    
-    if(command == "write" && args.size() != 4){
-        cerr << "Too few args: " << endl;
         print_usage(args[0]);
         return EXIT_FAILURE;
     }
@@ -54,7 +60,6 @@ int main(int argc,const char *argv[]){
         .sin_port   = htons(PORT),
         .sin_addr   = sin_addr,
     };
-    // struct sockaddr_in server_addr;
 
     int client = socket(AF_INET, SOCK_STREAM, 0);
     if(client == -1){
@@ -70,24 +75,21 @@ int main(int argc,const char *argv[]){
 
     if(command == "read"){
         snprintf(buf,sizeof(buf),"read \r\n");
-        send(client, buf, strlen(buf),0);
-        ssize_t n = recv(client,buf,1024,0);
-        string cont(buf,n);
-        cout << cont << endl;
-        // TODO large data
-        /*while(n>0){
-            cout << buf;
-            n = recv(client,buf,1024,0);
-        }*/
+    }else if(command == "write"){
+        snprintf(buf,sizeof(buf),"%s\r\n%s","write",argv[3]);
+    }else if(command == "insert"){
+        try {
+            stoll(args[4]);
+        }catch(invalid_argument){
+            cerr << "Invalid number: " << args[4] << endl;
+            return EXIT_FAILURE;
+        }
+        // position is limited to 64bit integer
+        snprintf(buf,sizeof(buf),"%s\n%20s\r\n%s","insert",argv[4],argv[3]);
     }
-    else if(command == "write"){
-        snprintf(buf,sizeof(buf),"%s\r\n%s\r\n","write",argv[3]);
-        send(client, buf, strlen(buf),0);
-        //cout <<"send:"<< buf << ":send" << endl;
-        ssize_t n = recv(client,buf,1024,0);
-        string cont(buf,n);
-        cout << cont << endl;
-        //cout <<"reply:"<< cont << ":reply" << endl;
-        //cout << n << endl;
-    }
+    send(client, buf, strlen(buf),0);
+    ssize_t n = recv(client,buf,1024,0);
+    string cont(buf,n);
+    cout << cont << endl;
+    // TODO: support large data
 }
